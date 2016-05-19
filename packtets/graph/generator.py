eba7d3d354da9bef50bfdbc48e6934c4e17f165c
Max Hutchinson <maxhutch@gmail.com>
2016-05-19 18:10:40 -0500

    Need to periodically mirror both tets when checking collisions.

--- packtets/graph/generator.py
+++ packtets/graph/generator.py
@@ -1,5 +1,14 @@
 import igraph
 
+def tet_collision(t1, t2, vx, vy, vz):
+    sym1 = t1.get_symmetry(vx, vy, vz)
+    sym2 = t2.get_symmetry(vx, vy, vz)
+    for s1 in sym1:
+        for s2 in sym2:
+            if s1.collision(s2):
+                return True
+    return False
+
 def packing_graph(tets, vx, vy, vz, independent = 0):
     N = len(tets)
     for i in range(N-1, independent-1, -1):
@@ -12,10 +21,8 @@ def packing_graph(tets, vx, vy, vz, independent = 0):
     g = igraph.Graph()
     g.add_vertices(N)
     for i in range(N):
-        for j in range(max(i, independent),N):
-            for s in tets[j].get_symmetry(vx, vy, vz, include_self=(i != j)):
-                if tets[i].collision(s):
-                    g.add_edge(i,j)
-                    break
+        for j in range(max(i+1, independent),N):
+            if tet_collision(tets[i], tets[j], vx, vy, vz):
+                g.add_edge(i,j)
 
     return g
